26e7e0831fd7d962eddf1fe1bdc14b02c085e5f8
Tudor-Dan Ravoiu <tudor2004@yahoo.com>
2022-01-29 10:28:39 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import React from "react"
 
+import { Popover } from "@headlessui/react"
+
+const Button = props => {
+  return <button type="button">{props.children}</button>
+}
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Popover>
+        {({ open }) => (
+          <>
+            <Popover.Button as="a">
+              <span>Solutions</span>
+            </Popover.Button>
+
+            <Popover.Panel tabIndex={-1}>
+              <div>
+                <Button>First Button</Button>
+                <Button>Second Button</Button>
+              </div>
+            </Popover.Panel>
+          </>
+        )}
+      </Popover>
+    </div>
+  )
 }
